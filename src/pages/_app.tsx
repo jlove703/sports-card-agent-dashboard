f618de9cb0544fedfaa74feb50a095b991a2845e
Jlove703 <jlove703@gmail.com>
2025-06-13 07:41:19 -0400

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
-import type { AppProps } from 'next/app'
 import '../app/globals.css'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
